ab2b160ff0a48258e9e0f021ce3ec777982afc6c
Yoyo Yang <yoyo.truly@gmail.com>
2022-03-24 22:01:48 -0400

    Add eqArrays function to compare one level arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  if (!arr1.length && !arr2.length) return true;
+  
+  let result = false;
+  for (let i = 0; i < arr1.length; i++) {
+    result = (arr1[i] === arr2[i]);
+  }
+
+  return result;
+};
+
+
+// Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
