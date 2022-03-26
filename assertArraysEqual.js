a96313d894de0bb95cbefdb5cdc2d567bf790fb3
Yoyo Yang <yoyo.truly@gmail.com>
2022-03-26 16:59:27 -0400

    Refactor assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,19 @@
-const assertArraysEqual = function(arr1, arr2) {
-  const failMsg = `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
-  const passMsg = `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
-
-  if (arr1.length !== arr2.length) return console.log(failMsg);
-  if (!arr1.length && !arr2.length) return console.log(passMsg);
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  if (!arr1.length && !arr2.length) return true;
   
   let isEqual = false;
   for (let i = 0; i < arr1.length; i++) {
     isEqual = (arr1[i] === arr2[i]);
   }
-  isEqual ? console.log(passMsg) : console.log(failMsg);
+
+  return isEqual;
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([], []);
+const assertArraysEqual = function(arr1, arr2) {
+  const failMsg = `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
+  const passMsg = `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
+
+  eqArrays(arr1, arr2) ? console.log(passMsg) : console.log(failMsg);
+};
