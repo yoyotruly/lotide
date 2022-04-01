ad590399c9396c3ea01a94751abe47d95b51b708
Yoyo Yang <yoyo.truly@gmail.com>
2022-03-31 20:39:41 -0400

    Add comparison for arrays as values in eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -15,17 +15,24 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1).sort();
-  const keys2 = Object.keys(object2).sort();
+const eqObjects = function(obj1, obj2) {
+  const keys1 = Object.keys(obj1).sort();
+  const keys2 = Object.keys(obj2).sort();
 
   if (!eqArrays(keys1, keys2)) return false;
   
-  return keys1.every(key => object1[key] === object2[key]);
+  return keys1.every(key => {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    return Array.isArray(val1) && Array.isArray(val2) ?
+      eqArrays(val1, val2) :
+      val1 === val2;
+  });
 };
 
 
-// test cases
+// test cases - primitive types as values
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3"};
@@ -34,3 +41,13 @@ const abc2 = { a: "1", b: "3", c: "3"};
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(abc, abc2), false);
+
+// test cases - arrays as values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd3 = { c: ["1"], d: ["2", 3] };
+
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
+assertEqual(eqObjects(cd, cd3), false);
