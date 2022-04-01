82218b97ba1a3bc965393665f36c70274ba97bad
Yoyo Yang <yoyo.truly@gmail.com>
2022-03-31 20:16:24 -0400

    Fix bug in eqArrays function and add test case for it

--- eqArrays.js
+++ eqArrays.js
@@ -11,12 +11,7 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   if (!arr1.length && !arr2.length) return true;
   
-  let isEqual = false;
-  for (let i = 0; i < arr1.length; i++) {
-    isEqual = (arr1[i] === arr2[i]);
-  }
-
-  return isEqual;
+  return arr1.every((val, index) => val === arr2[index]);
 };
 
 
@@ -24,6 +19,7 @@ const eqArrays = function(arr1, arr2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([], []), true);
