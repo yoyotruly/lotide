f812d49327ad4cb01c8f4b9b4c24d46c7be96d5d
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-01 16:40:29 -0400

    Clean up unused code

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,3 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
