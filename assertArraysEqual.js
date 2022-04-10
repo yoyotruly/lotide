f27ae9ef558a10ad6a8dd7a5d1f6764264b26ffb
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-10 15:47:55 -0400

    Refactor module import and export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  if (!arr1.length && !arr2.length) return true;
-  
-  return arr1.every((val, index) => val === arr2[index]);
-};
-
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(actual, expected) {
   const failMsg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
@@ -12,3 +6,5 @@ const assertArraysEqual = function(actual, expected) {
 
   eqArrays(actual, expected) ? console.log(passMsg) : console.log(failMsg);
 };
+
+module.exports = assertArraysEqual;
