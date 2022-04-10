2267d39c3f7c09feb6fde60e3bc80e4ce5f5ebfc
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-10 15:51:49 -0400

    Remove unused modules from middle.js

--- middle.js
+++ middle.js
@@ -1,6 +1,3 @@
-const eqArrays = require("./eqArrays");
-const assertArraysEqual = require("./assertArraysEqual");
-
 const middle = function(arr) {
   let outputArr = [];
   const len = arr.length;
