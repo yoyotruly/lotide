d5fba7ca79493c9a1c7a6bdc9bc015edc71681d8
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-10 16:04:13 -0400

    Refactor headTest to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,36 @@
-const assertEqual = require("../assertEqual");
+const { expect } = require("chai");
+
 const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "World"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+
+describe("head()", () => {
+
+  it("should return 1 when passed [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const expectedOutput = 1
+
+    expect(head(input)).to.equal(expectedOutput);
+  });
+
+  it("should return 'Hello' when passed ['Hello', 'World']", () => {
+    const input = ["Hello", "World"];
+    const expectedOutput = "Hello";
+
+    expect(head(input)).to.equal(expectedOutput);
+  });
+
+  it("should return 5 when passed [5]", () => {
+    const input = [5];
+    const expectedOutput = 5
+
+    expect(head(input)).to.equal(expectedOutput);
+  });
+
+  it("should return undefined if passed []", () => {
+    const input = [];
+    const expectedOutput = undefined;
+
+    expect(head(input)).to.equal(expectedOutput);
+  })
+
+});
