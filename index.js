7e4fc32207beab9a53ca8e5e3a783b4cc10d33e1
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-10 20:45:22 -0400

    Add index.js to export all functions as an object

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const without = require("./without");
+const flatten = require("./flatten");
+const countOnly = require("./countOnly");
+const countLetters = require("./countLetters");
+const letterPositions = require("./letterPositions");
+const findKeyByValue = require("./findKeyByValue");
+const findKey = require("./findKey");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  without,
+  flatten,
+  countOnly,
+  countLetters,
+  letterPositions,
+  findKeyByValue,
+  findKey,
+  map,
+  takeUntil
+};
