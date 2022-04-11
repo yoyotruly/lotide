1be5097afa237eb644a89389ecb920de92f57dbd
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-10 20:32:58 -0400

    Refactor middleTest using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,36 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const { expect } = require("chai");
+
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("#middle()", () => {
+
+  it("should return [] when passed [1]", () => {
+    const input = [1];
+    const expectedOutput = [];
+
+    expect(middle(input)).to.deep.equal(expectedOutput);
+  });
+
+  it("should return [] when passed [1, 2]", () => {
+    const input = [1, 2];
+    const expectedOutput = [];
+
+    expect(middle(input)).to.deep.equal(expectedOutput);
+  });
+
+  it("should return [2] when passed [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const expectedOutput = [2];
+
+    expect(middle(input)).to.deep.equal(expectedOutput);
+  });
+
+  it("should return [3, 4] when passed [1, 2, 3, 4, 5, 6]", () => {
+    const input = [1, 2, 3, 4, 5, 6];
+    const expectedOutput = [3, 4];
+
+    expect(middle(input)).to.deep.equal(expectedOutput);
+  })
+
+});
