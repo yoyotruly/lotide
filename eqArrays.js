c14336063328716c5e6deb78bc1a5a8d7692b72a
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 16:56:56 -0400

    Add eqArrays docstring

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,23 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  if (!arr1.length && !arr2.length) return true;
+/**
+ * Takes in two arrays and returns true or false based on a perfect match.
+ * @param {Array} array1 The first array to compare
+ * @param {Array} array2 The second array to compare
+ * @returns {boolean} Returns `true` if the arrays are equivalent, else `false`.
+ * @example
+ *  * eqArrays([1, 2], [1, 2])
+ * // => true
+ *
+ *  * eq([1, 2], [1, "2"])
+ * // => false
+ *
+ *  * eqArrays([], [])
+ * // => true
+ */
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  if (!array1.length && !array2.length) return true;
   
-  return arr1.every((val, index) => val === arr2[index]);
+  return array1.every((val, index) => val === array2[index]);
 };
 
 module.exports = eqArrays;
