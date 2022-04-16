9670c7f91b7ff17eda158624b3c7c1607aa582a2
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 17:29:11 -0400

    Add docstring for tail

--- tail.js
+++ tail.js
@@ -1,5 +1,17 @@
-const tail = function(arr) {
-  return arr.slice(1);
+/**
+ * Retrieves every element except the first element (head) of the array.
+ * @param {Array} array The array to retrieve from.
+ * @returns {Array} Returns a slice of the array. Returns an empty array if `array` is empty or only has one element.
+ * @example
+ *
+ * _.tail([1, 2, 3])
+ * // => [2, 3]
+ *
+ * _.tail([1])
+ * // => []
+ */
+const tail = function(array) {
+  return array.slice(1);
 };
 
 module.exports = tail;
