6dcacc573a37dd7816c36a02b83fcd19c5bb1c67
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 17:25:44 -0400

    Refactor without using array method

--- without.js
+++ without.js
@@ -1,13 +1,15 @@
-const without = function(sourceArr, itemsToRemoveArr) {
-  let outputArr = [];
-
-  for (const item of sourceArr) {
-    if (!itemsToRemoveArr.includes(item)) {
-      outputArr.push(item);
-    }
-  }
-
-  return outputArr;
+/**
+ * Returns a subset of an array with unwanted elements removed.
+ * @param {Array} array The input array
+ * @param {Array} itemsToRemove The item to remove from the input array
+ * @returns {Array} Returns a slice of the array with items removed
+ * @example
+ *
+ * _.without([1, 2, 3], [2, 4])
+ * // => [ 1, 3 ]
+ */
+const without = function(array, itemsToRemove) {
+  return array.filter(item => !itemsToRemove.includes(item));
 };
 
 module.exports = without;
