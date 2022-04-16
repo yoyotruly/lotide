15883e09c444a35e4fbdf46879afe3272378511b
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 16:23:16 -0400

    Add assertEqual docstring

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,17 @@
+/**
+ * Tests for equality between two values of primitive types and prints out a colorful message to inform
+ * if the assertion passes or fails.
+ * @param {*} actual The actual value
+ * @param {*} expected The expected value
+ * @returns {undefined} undefined. Success or failure message is printed out to console.
+ * @example
+ *
+ * _.assertEqual(1.2, 1.2);
+ * // console prints "✅✅✅ Assertion Passed: 1.2 === 1.2"
+ *
+ * _.assertEqual(true, false);
+ * // console prints "🛑🛑🛑 Assertion Failed: true !== false"
+ */
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
