8e7d08461fcca9a527607d846f0337085af42364
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 16:33:21 -0400

    Add countLetters docstring

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,22 @@
-const countLetters = function(sentence) {
-  const results = {};
+/**
+ * Given a string, returns an object containing counts of each of the letters in that string.
+ * @param {string} string The string to count from
+ * @returns {Object} an object containing counts of each letter in that string
+ * @example
+ *
+ * _.countLetters("big dog");
+ * // => { b: 1, i: 1, g: 2, d: 1, o: 1 }
+ */
+const countLetters = function(string) {
+  const result = {};
 
-  for (const letter of sentence) {
+  for (const letter of string) {
     if (letter !== " ") {
-      results[letter] ? results[letter] += 1 : results[letter] = 1;
+      result[letter] ? result[letter] += 1 : result[letter] = 1;
     }
   }
 
-  return results;
+  return result;
 };
 
 module.exports = countLetters;
