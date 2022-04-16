12542225cdd86c6149c6a912dffac60278077f98
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 16:27:48 -0400

    Add assertObjectsEqual docstring

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,18 @@
 const eqObjects = require("./eqObjects");
 
-
+/**
+ * Tests for equality between two objects and prints out a colorful message to inform if the assertion passes or fails.
+ * @param {Object} actual The actual object
+ * @param {Object} expected The expected object
+ * @returns {undefined} undefined. Success or failure message is printed out to console.
+ * @example
+ *
+ * _.assertObjectsEqual({name: "john", surname: "smith"}, {name: "john", surname: "smith"});
+ * // console prints "✅✅✅ Assertion Passed: { name: 'john', surname: 'smith' } === { name: 'john', surname: 'smith' }"
+ *
+ * _.assertObjectsEqual({name: "john", surname: "smith"}, {name: "jane"});
+ * // console prints "🛑🛑🛑 Assertion Failed: { name: 'john', surname: 'smith' } !== { name: 'jane' }"
+ */
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
 
