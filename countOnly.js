1fd84dcc9373b4010c5f9c4654a035b3682158b5
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 16:42:01 -0400

    Add countOnly docstring

--- countOnly.js
+++ countOnly.js
@@ -1,13 +1,23 @@
+/**
+ * Given an array and an object, returns an object containing counts of everything that an input object listed.
+ * @param {Array} allItems The array of strings to look through
+ * @param {Object} itemsToCount The object specifying what to count
+ * @returns {Object} An object containing counts of everything that the input object listed
+ * @example
+ *
+ * _.countOnly(["a", "b", "c", "b"], { a: true, b: true, c: false, d: true });
+ * // => { a: 1, b: 2 }
+ */
 const countOnly = function(allItems, itemsToCount) {
-  const results = {};
+  const result = {};
   
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      results[item] ? results[item] += 1 : results[item] = 1;
+      result[item] ? result[item] += 1 : result[item] = 1;
     }
   }
   
-  return results;
+  return result;
 };
 
 module.exports = countOnly;
