01a98d2c4c32645e2746de8f99cbf82cf77f7dca
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 19:56:49 -0400

    Add docstring for middle

--- middle.js
+++ middle.js
@@ -1,15 +1,26 @@
-const middle = function(arr) {
-  let outputArr = [];
-  const len = arr.length;
-  
-  if (len < 3) return outputArr;
+/**
+ * Retrieves the middle-most element(s) of an array.
+ * @param {Array} array The array to retrieve from.
+ * @returns {Array} Returns the middle-most element(s) of the array.
+ * @example
+ *
+ * _.middle([1, 2, 3])
+ * // => [ 2 ]
+ *
+ * _.middle([1, 2, 3, 4])
+ * // => [ 2, 3 ]
+ *
+ * _.middle([1])
+ * // => []
+ */
+const middle = function(array) {
+  const len = array.length;
+  if (len < 3) return [];
   
   const idx = len / 2;
-  len % 2 === 0 ?
-    outputArr.push(arr[idx - 1], arr[idx]) :
-    outputArr.push(arr[Math.floor(idx)]);
+  if (len % 2 === 0) return [array[idx - 1], array[idx]];
   
-  return outputArr;
+  return [array[Math.floor(idx)]];
 };
 
 module.exports = middle;
