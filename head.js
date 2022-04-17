e100f8ef5e75194f8d518522cb6e3487c6435ab7
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 20:07:33 -0400

    Add docstring for head

--- head.js
+++ head.js
@@ -1,3 +1,15 @@
+/**
+ * Retrieves the first element from the array.
+ * @param {Array} array - The array to query.
+ * @returns {*} Returns the first element of `array`.
+ * @example
+ *
+ * _.head([1, 2, 3]);
+ * // => 1
+ *
+ * _.head([]);
+ * // => undefined
+ */
 const head = function(arr) {
   return arr[0];
 };
