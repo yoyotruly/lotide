113bca34578d664f8f9340464d9055e8f5d01466
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-16 20:21:20 -0400

    Add docstring for letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,14 +1,22 @@
-const letterPositions = function(sentence) {
-  const results = {};
-
-  for (let i = 0; i < sentence.length; i++) {
-    const letter = sentence[i];
+/**
+ * Returns all the indices in a string where each character is found.
+ * @param {string} string The string to look through.
+ * @returns {Object} Returns an object with each character as key and its indices as values.
+ * @example
+ *
+ * _.letterPositions("aba a")
+ * => { a: [ 0, 2, 4 ], b: [ 1 ] }
+ */
+const letterPositions = function(string) {
+  const result = {};
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i];
     if (letter !== " ") {
-      results[letter] ? results[letter].push(i) : results[letter] = [i];
+      result[letter] ? result[letter].push(i) : result[letter] = [i];
     }
   }
   
-  return results;
+  return result;
 };
 
 module.exports = letterPositions;
